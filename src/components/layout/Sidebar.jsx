c96c5ca5cbd2f27a961f3a856992d05020355d08
sheikh ariful <sheikharifulislam770@gmail.com>
2022-07-02 03:16:18 +0600

    minimize class name

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -9,28 +9,28 @@ import { FiHome, FiMenu } from "react-icons/fi";
 
 const Sidebar = () => {
     return (
-        <aside className="h-screen border border-purple-900 bg-white px-2 pt-7 dark:bg-[#2E1619] sm:pt-9">
+        <aside className="h-screen bg-white px-2 pt-7 dark:bg-[#2E1619] sm:pt-9">
             <nav>
                 <ul className="space-y-6">
-                    <li className="group relative cursor-pointer overflow-hidden px-2">
-                        <FiMenu className="h-7 w-7 text-[#FF7594] group-hover:text-hot-pink" />
+                    <li className="li group">
+                        <FiMenu className="li-icon text-[#FF7594]" />
                     </li>
-                    <li className="group relative cursor-pointer overflow-hidden px-2">
-                        <FiHome className="h-7 w-7 text-[#FF7594] group-hover:text-hot-pink" />
+                    <li className="li group">
+                        <FiHome className="li-icon text-[#FF7594]" />
                     </li>
-                    <li className="group relative cursor-pointer overflow-hidden px-2">
-                        <BiUser className="h-7 w-7 group-hover:text-hot-pink" />
+                    <li className="li group">
+                        <BiUser className="li-icon" />
                     </li>
-                    <li className="group relative cursor-pointer overflow-hidden px-2">
-                        <BsCalendar4Week className="h-7 w-7 group-hover:text-hot-pink" />
+                    <li className="li group">
+                        <BsCalendar4Week className="li-icon" />
                     </li>
-                    <li className="group relative cursor-pointer overflow-hidden px-2">
-                        <BsArrowCounterclockwise className="h-7 w-7 group-hover:text-hot-pink" />
+                    <li className="li group">
+                        <BsArrowCounterclockwise className="li-icon" />
                     </li>
-                    <li className="group relative cursor-pointer overflow-hidden px-2">
-                        <BsGear className="h-7 w-7 group-hover:text-hot-pink" />
+                    <li className="li group">
+                        <BsGear className="li-icon" />
                     </li>
-                    <li className="group relative cursor-pointer overflow-hidden px-2">
+                    <li className="li group">
                         <button className="mt-16 rounded-full bg-gradient-to-r from-hot-pink to-orange-peach">
                             <BsPlus className=" box-content h-7 w-7 rounded-full bg-white bg-opacity-20 p-1 group-hover:text-hot-pink" />
                         </button>
